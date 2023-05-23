5e72956cc5c9e8938c5a9e13708777005c1a049c
saniul06 <saniul428@gmail.com>
2023-05-23 12:24:59 +0600

    Add currentUser property as User entity to the Request object of express package

--- src/middlewares/current-user.middleware.ts
+++ src/middlewares/current-user.middleware.ts
@@ -1,6 +1,15 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
+import { User } from "src/users/user.entity";
 import { UsersService } from "src/users/users.service";
+
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: User;
+        }
+    }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -10,7 +19,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
         const { userId } = req.session || {};
         if (userId) {
             const user = await this.userService.findOne(userId);
-            // @ts-ignore
             req.currentUser = user;
         }
         next();
